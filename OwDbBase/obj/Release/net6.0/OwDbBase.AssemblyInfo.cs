4f3e5a70c8b0824a0b8c0ccf289166b3e10d7795
ourworld_cn <ourworld_cn@msn.com>
2023-11-30 17:34:16 +0800

    日提交

--- OwDbBase/obj/Release/net6.0/OwDbBase.AssemblyInfo.cs
+++ OwDbBase/obj/Release/net6.0/OwDbBase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OwDbBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d9e26f31c9bb38b11229512c42fb3300971baa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OwDbBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OwDbBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
