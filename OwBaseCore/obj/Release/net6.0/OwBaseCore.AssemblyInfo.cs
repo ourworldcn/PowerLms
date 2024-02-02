5e5da532e27cc2857ea1d673915a023bf596eea4
ourworld_cn <ourworld_cn@msn.com>
2024-02-02 18:07:01 +0800

    日提交

--- OwBaseCore/obj/Release/net6.0/OwBaseCore.AssemblyInfo.cs
+++ OwBaseCore/obj/Release/net6.0/OwBaseCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc345a793f611e78038c215763103b47436c240c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OwBaseCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OwBaseCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
