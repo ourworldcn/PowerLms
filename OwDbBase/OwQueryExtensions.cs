e61635854b4be4a1fd65daf71f9926b77e18aad4
ourworld_cn <ourworld_cn@msn.com>
2024-05-06 18:00:50 +0800

    对And类型的多条件查询进行通用支持

--- OwDbBase/OwQueryExtensions.cs
+++ OwDbBase/OwQueryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -105,6 +106,144 @@ namespace OW.Data
             var l = Expression.GreaterThanOrEqual(property, min);
             var r = Expression.LessThanOrEqual(property, max);
             return Expression.AndAlso(l, r);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="queryable"></param>
+        /// <param name="conditional"></param>
+        /// <returns>返回的可查询接口，null表示出错。</returns>
+        public static IQueryable<T> GenerateWhereAnd<T>(IQueryable<T> queryable, IDictionary<string, string> conditional) where T : class
+        {
+            IQueryable<T> result = queryable;
+            var type = typeof(T);
+            var para = Expression.Parameter(type);
+            foreach (var item in conditional)
+            {
+                if (type.GetProperty(item.Key, BindingFlags.IgnoreCase | BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy) is null) continue;
+                var left = Expression.Property(para, item.Key);
+                var values = item.Value.Split(',');
+                Expression body;
+                if (values.Length == 1)
+                {
+                    var right = Constant(values[0], left.Type);
+                    if (typeof(string) == left.Type) //对字符串则使用模糊查找
+                    {
+                        body = StringContains(left, Constant(values[0], left.Type));
+
+                    }
+                    else
+                        body = Expression.Equal(left, Constant(values[0], left.Type));
+                }
+                else if (values.Length == 2)
+                {
+                    body = Between(left, Constant(values[0], left.Type), Constant(values[1], left.Type));
+                }
+                else
+                {
+                    OwHelper.SetLastErrorAndMessage(404, $"不正确的参数格式——{item.Value}。");
+                    return null;
+                }
+                var func = Expression.Lambda<Func<T, bool>>(body, para);
+                result = result.Where(func);
+            }
+            return result;
+        }
+
+        public static Expression Constant(string value, Type type)
+        {
+            Expression result;
+            var innerType = type;
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))  //可空类型
+                innerType = type.GetGenericArguments()[0];
+            switch (Type.GetTypeCode(innerType))
+            {
+                case TypeCode.Empty:
+                case TypeCode.DBNull:
+                    result = Expression.Constant(null, type);
+                    break;
+                case TypeCode.Object:
+                    if (innerType == typeof(Guid))
+                    {
+                        if (string.Equals(value, "null", StringComparison.OrdinalIgnoreCase))
+                            result = Expression.Constant(null, type);
+                        else if (!Guid.TryParse(value, out Guid guidValue))
+                        {
+                            OwHelper.SetLastErrorAndMessage(404, $"不能转换为类型{type}, {value}");
+                            return null;
+                        }
+                        else
+                            result = Expression.Constant(guidValue, type);
+                    }
+                    else
+                    {
+                        OwHelper.SetLastErrorAndMessage(404, $"不能转换为类型{type}, {value}");
+                        return null;
+                    }
+                    break;
+                case TypeCode.Boolean:
+                    if (string.Equals(value, "null", StringComparison.OrdinalIgnoreCase))
+                        result = Expression.Constant(null, type);
+                    else if (!bool.TryParse(value, out bool boolValue))
+                    {
+                        OwHelper.SetLastErrorAndMessage(404, $"不能转换为类型{type}, {value}");
+                        return null;
+                    }
+                    else
+                        result = Expression.Constant(boolValue, type);
+                    break;
+                case TypeCode.Char:
+                    if (string.Equals(value, "null", StringComparison.OrdinalIgnoreCase))
+                        result = Expression.Constant(null, type);
+                    else if (!char.TryParse(value, out char charValue))
+                    {
+                        OwHelper.SetLastErrorAndMessage(404, $"不能转换为类型{type}, {value}");
+                        return null;
+                    }
+                    else result = Expression.Constant(charValue, type);
+                    break;
+                case TypeCode.SByte:
+                case TypeCode.Byte:
+                case TypeCode.Int16:
+                case TypeCode.UInt16:
+                case TypeCode.Int32:
+                case TypeCode.UInt32:
+                case TypeCode.Int64:
+                case TypeCode.UInt64:
+                case TypeCode.Single:
+                case TypeCode.Double:
+                case TypeCode.Decimal:
+                    if (string.Equals(value, "null", StringComparison.OrdinalIgnoreCase))
+                        result = Expression.Constant(null, type);
+                    else if (!decimal.TryParse(value, out decimal decimalValue))
+                    {
+                        OwHelper.SetLastErrorAndMessage(404, $"不能转换为类型{type}, {value}");
+                        return null;
+                    }
+                    else result = Expression.Convert(Expression.Constant(decimalValue), type);
+                    break;
+                case TypeCode.DateTime:
+                    if (string.Equals(value, "null", StringComparison.OrdinalIgnoreCase))
+                        result = Expression.Constant(null, type);
+                    else if (!DateTime.TryParse(value, out DateTime dateTimeValue))
+                    {
+                        OwHelper.SetLastErrorAndMessage(404, $"不能转换为类型{type}, {value}");
+                        return null;
+                    }
+                    else result = Expression.Constant(dateTimeValue, type);
+                    break;
+                case TypeCode.String:
+                    if (string.Equals(value, "null", StringComparison.OrdinalIgnoreCase))
+                        result = Expression.Constant(null, type);
+                    else result = Expression.Constant(value, type);
+                    break;
+                default:
+                    OwHelper.SetLastErrorAndMessage(404, $"不能转换为类型{type}, {value}");
+                    return null;
+            }
+            return result;
         }
     }
 }
