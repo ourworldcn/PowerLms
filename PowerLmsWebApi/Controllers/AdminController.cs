69ea23f9782baab5596b42b344a4364a65a0cc1f
ourworld_cn <ourworld_cn@msn.com>
2024-08-08 17:35:42 +0800

    增加获取系统日志接口：Admin/GetAllSystemLog 。目前仅能查询，有需求再补。

--- PowerLmsWebApi/Controllers/AdminController.cs
+++ PowerLmsWebApi/Controllers/AdminController.cs
@@ -2066,7 +2066,44 @@ namespace PowerLmsWebApi.Controllers
         }
 
         #endregion 箱型相关
+
+        #region 日志相关
+        /// <summary>
+        /// 获取系统日志。
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="conditional">已支持通用查询——除个别涉及敏感信息字段外，所有实体字段都可作为条件。</param>
+        /// <returns></returns>
+        /// <response code="200">未发生系统级错误。但可能出现应用错误，具体参见 HasError 和 ErrorCode 。</response>  
+        /// <response code="401">无效令牌。</response>  
+        [HttpGet]
+        public ActionResult<GetAllSystemLogReturnDto> GetAllSystemLog([FromQuery] PagingParamsDtoBase model,
+            [FromQuery] Dictionary<string, string> conditional = null)
+        {
+            if (_AccountManager.GetAccountFromToken(model.Token, _ServiceProvider) is not OwContext context) return Unauthorized();
+            var result = new GetAllSystemLogReturnDto();
+
+            var dbSet = _DbContext.OwSystemLogs;
+            var coll = dbSet.OrderBy(model.OrderFieldName, model.IsDesc).AsNoTracking();
+            coll = EfHelper.GenerateWhereAnd(coll, conditional);
+            var prb = _EntityManager.GetAll(coll, model.StartIndex, model.Count);
+            _Mapper.Map(prb, result);
+            return result;
+        }
+
+        #endregion 日志相关
     }
+
+    #region 日志相关
+
+    /// <summary>
+    /// 获取系统日志功能的返回值封装类。
+    /// </summary>
+    public class GetAllSystemLogReturnDto: PagingReturnDtoBase<ContainerKindCount>
+    {
+    }
+
+    #endregion 日志相关
 
     #region 箱型相关
 
