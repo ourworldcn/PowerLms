f551fe6be0f3ff867b9ee66f753160f72a00906b
ourworld_cn <ourworld_cn@msn.com>
2025-04-25 20:55:17 +0800

    增加PooledListBase<T> 和 PooledList<T>类

---
+++ OwBaseCore/System.Collections.Generic/PooledList.cs
@@ -0,0 +1,302 @@
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// 一个类似 List&lt;T&gt; 的泛型集合类，其内部存储数组通过 ArrayPool&lt;T&gt;.Shared 进行池化分配与回收，
+    /// 以减少频繁分配大数组带来的 GC 压力和内存碎片，适用于高性能场景下大量临时数据的收集与处理。
+    /// </summary>
+    /// <remarks>
+    /// 注意：此类设计用于短生命周期场景，不适合长期持有。使用完毕后必须调用 Dispose 方法归还资源。
+    /// 典型用法是在 using 语句中使用此类的实例。
+    /// </remarks>
+    /// <typeparam name="T">集合中元素的类型</typeparam>
+    public sealed class PooledList<T> : PooledListBase<T>
+    {
+        private const int DefaultCapacity = 8; // 默认的初始容量
+
+        #region 构造函数
+        /// <summary>初始化 PooledList&lt;T&gt; 类的新实例，具有指定的初始容量。</summary>
+        /// <param name="capacity">初始容量，默认为 8</param>
+        /// <exception cref="ArgumentOutOfRangeException">capacity 小于 0</exception>
+        public PooledList(int capacity = DefaultCapacity) : base(Math.Max(capacity, DefaultCapacity))
+        {
+        }
+
+        /// <summary>初始化 PooledList&lt;T&gt; 类的新实例，该实例包含从指定集合复制的元素</summary>
+        /// <param name="collection">一个集合，其元素被复制到新列表中</param>
+        /// <exception cref="ArgumentNullException">collection 为 null</exception>
+        public PooledList(IEnumerable<T> collection) : base(collection is ICollection<T> c ? c.Count : DefaultCapacity)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            AddRange(collection);
+        }
+        #endregion
+
+        #region 增强的List<T>兼容方法
+        /// <summary>搜索与指定谓词匹配的元素，并返回整个 PooledList&lt;T&gt; 中第一个匹配元素的从零开始的索引</summary>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到第一个与指定谓词匹配的元素，则为该元素的从零开始的索引，否则为 -1</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        public int FindIndex(Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            return FindIndex(0, Count, match);
+        }
+
+        /// <summary>从指定的索引开始搜索与指定谓词匹配的元素，并返回整个 PooledList&lt;T&gt; 中第一个匹配元素的从零开始的索引</summary>
+        /// <param name="startIndex">从零开始的搜索起始索引</param>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到第一个与指定谓词匹配的元素，则为该元素的从零开始的索引，否则为 -1</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">startIndex 超出范围</exception>
+        public int FindIndex(int startIndex, Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            if (startIndex < 0 || startIndex > Count) throw new ArgumentOutOfRangeException(nameof(startIndex));
+            return FindIndex(startIndex, Count - startIndex, match);
+        }
+
+        /// <summary>从指定的索引开始搜索指定数量的元素中与指定谓词匹配的元素，并返回整个 PooledList&lt;T&gt; 中第一个匹配元素的从零开始的索引</summary>
+        /// <param name="startIndex">从零开始的搜索起始索引</param>
+        /// <param name="count">要搜索的部分中的元素数</param>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到第一个与指定谓词匹配的元素，则为该元素的从零开始的索引，否则为 -1</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">startIndex 或 count 超出范围</exception>
+        public int FindIndex(int startIndex, int count, Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            if (startIndex < 0 || startIndex > Count) throw new ArgumentOutOfRangeException(nameof(startIndex));
+            if (count < 0 || startIndex + count > Count) throw new ArgumentOutOfRangeException(nameof(count));
+
+            int endIndex = startIndex + count;
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (match(this[i])) return i;
+            }
+            return -1;
+        }
+
+        /// <summary>搜索与指定谓词定义的条件相匹配的元素，并返回 PooledList&lt;T&gt; 中的第一个匹配元素</summary>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到与指定谓词定义的条件相匹配的第一个元素，则为该元素；否则为类型 T 的默认值</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        public T Find(Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            for (int i = 0; i < Count; i++)
+            {
+                if (match(this[i])) return this[i];
+            }
+            return default;
+        }
+
+        /// <summary>检索与指定谓词定义的条件相匹配的所有元素</summary>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到与指定谓词定义的条件相匹配的元素，则为这些元素组成的 PooledList&lt;T&gt;；否则为空的 PooledList&lt;T&gt;</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        public PooledList<T> FindAll(Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            PooledList<T> list = new PooledList<T>();
+            for (int i = 0; i < Count; i++)
+            {
+                if (match(this[i])) list.Add(this[i]);
+            }
+            return list;
+        }
+
+        /// <summary>搜索与指定谓词匹配的元素，并返回整个 PooledList&lt;T&gt; 中最后一个匹配元素的从零开始的索引</summary>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到最后一个与指定谓词匹配的元素，则为该元素的从零开始的索引，否则为 -1</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        public int FindLastIndex(Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            return FindLastIndex(Count - 1, Count, match);
+        }
+
+        /// <summary>从指定的索引开始向前搜索与指定谓词匹配的元素，并返回整个 PooledList&lt;T&gt; 中最后一个匹配元素的从零开始的索引</summary>
+        /// <param name="startIndex">从零开始的向后搜索的起始索引</param>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到最后一个与指定谓词匹配的元素，则为该元素的从零开始的索引，否则为 -1</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">startIndex 超出范围</exception>
+        public int FindLastIndex(int startIndex, Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            if (Count == 0) return -1;
+            if (startIndex >= Count) throw new ArgumentOutOfRangeException(nameof(startIndex));
+            return FindLastIndex(startIndex, startIndex + 1, match);
+        }
+
+        /// <summary>从指定索引开始向后搜索指定数量的元素中与指定谓词匹配的元素，并返回整个 PooledList&lt;T&gt; 中最后一个匹配元素的从零开始的索引</summary>
+        /// <param name="startIndex">从零开始的向后搜索的起始索引</param>
+        /// <param name="count">要搜索的部分中的元素数</param>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到最后一个与指定谓词匹配的元素，则为该元素的从零开始的索引，否则为 -1</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">startIndex 或 count 超出范围</exception>
+        public int FindLastIndex(int startIndex, int count, Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            if (Count == 0)
+            {
+                if (startIndex != -1) throw new ArgumentOutOfRangeException(nameof(startIndex));
+                return -1;
+            }
+            if (startIndex < 0 || startIndex >= Count) throw new ArgumentOutOfRangeException(nameof(startIndex));
+            if (count < 0 || startIndex - count + 1 < 0) throw new ArgumentOutOfRangeException(nameof(count));
+
+            int endIndex = startIndex - count + 1;
+            for (int i = startIndex; i >= endIndex; i--)
+            {
+                if (match(this[i])) return i;
+            }
+            return -1;
+        }
+
+        /// <summary>搜索与指定谓词定义的条件相匹配的元素，并返回 PooledList&lt;T&gt; 中的最后一个匹配元素</summary>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果找到与指定谓词定义的条件相匹配的最后一个元素，则为该元素；否则为类型 T 的默认值</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        public T FindLast(Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                if (match(this[i])) return this[i];
+            }
+            return default;
+        }
+
+        /// <summary>确定 PooledList&lt;T&gt; 中的每个元素是否与指定谓词定义的条件匹配</summary>
+        /// <param name="match">定义要搜索的元素的条件的谓词</param>
+        /// <returns>如果 PooledList&lt;T&gt; 中的每个元素都与指定谓词定义的条件匹配，则为 true；否则为 false</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        public bool TrueForAll(Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+            for (int i = 0; i < Count; i++)
+            {
+                if (!match(this[i])) return false;
+            }
+            return true;
+        }
+
+        /// <summary>对 PooledList&lt;T&gt; 中的每个元素执行指定操作</summary>
+        /// <param name="action">要对 PooledList&lt;T&gt; 的每个元素执行的委托</param>
+        /// <exception cref="ArgumentNullException">action 为 null</exception>
+        public void ForEach(Action<T> action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+            for (int i = 0; i < Count; i++) action(this[i]);
+        }
+
+        /// <summary>从指定索引开始，在 PooledList&lt;T&gt; 中搜索对象，并返回第一个匹配项的从零开始的索引</summary>
+        /// <param name="item">要在 PooledList&lt;T&gt; 中定位的对象，对于引用类型，该值可以为 null</param>
+        /// <param name="index">从零开始的搜索起始索引</param>
+        /// <returns>从 index 开始，如果在 PooledList&lt;T&gt; 中找到 item，则为该项的第一个匹配项的从零开始的索引；否则为 -1</returns>
+        /// <exception cref="ArgumentOutOfRangeException">index 超出范围</exception>
+        public int IndexOf(T item, int index)
+        {
+            if (index < 0 || index > Count) throw new ArgumentOutOfRangeException(nameof(index));
+            return Array.IndexOf(_Items, item, index, Count - index);
+        }
+
+        /// <summary>在 PooledList&lt;T&gt; 内，从指定的索引开始并包含指定计数的元素，搜索对象，并返回第一个匹配项的从零开始的索引</summary>
+        /// <param name="item">要在 PooledList&lt;T&gt; 中定位的对象，对于引用类型，该值可以为 null</param>
+        /// <param name="index">从零开始的搜索起始索引</param>
+        /// <param name="count">要搜索的区域中的元素数</param>
+        /// <returns>从 index 开始，在 count 个元素范围内，如果在 PooledList&lt;T&gt; 中找到 item，则为该项的第一个匹配项的从零开始的索引；否则为 -1</returns>
+        /// <exception cref="ArgumentOutOfRangeException">index 或 count 超出范围</exception>
+        public int IndexOf(T item, int index, int count)
+        {
+            if (index < 0 || index > Count) throw new ArgumentOutOfRangeException(nameof(index));
+            if (count < 0 || index + count > Count) throw new ArgumentOutOfRangeException(nameof(count));
+            return Array.IndexOf(_Items, item, index, count);
+        }
+
+        /// <summary>从 PooledList&lt;T&gt; 中移除与指定谓词定义的条件相匹配的所有元素</summary>
+        /// <param name="match">定义要移除的元素的条件的谓词</param>
+        /// <returns>从 PooledList&lt;T&gt; 中移除的元素数目</returns>
+        /// <exception cref="ArgumentNullException">match 为 null</exception>
+        public int RemoveAll(Predicate<T> match)
+        {
+            if (match == null) throw new ArgumentNullException(nameof(match));
+
+            int freeIndex = 0;
+            // 找到第一个要删除的元素
+            while (freeIndex < Count && !match(this[freeIndex])) freeIndex++;
+            if (freeIndex >= Count) return 0;
+
+            int current = freeIndex + 1;
+            while (current < Count)
+            {
+                while (current < Count && match(this[current])) current++;
+                if (current < Count) this[freeIndex++] = this[current++];
+            }
+
+            int removed = Count - freeIndex;
+            // 移除元素
+            for (int i = 0; i < removed; i++)
+                RemoveAt(Count - 1);
+
+            return removed;
+        }
+
+        /// <summary>从 PooledList&lt;T&gt; 中移除指定范围的元素</summary>
+        /// <param name="index">要移除的第一个元素的从零开始的索引</param>
+        /// <param name="count">要移除的元素数</param>
+        /// <exception cref="ArgumentOutOfRangeException">index 或 count 超出范围</exception>
+        public void RemoveRange(int index, int count)
+        {
+            if (index < 0 || index > Count) throw new ArgumentOutOfRangeException(nameof(index));
+            if (count < 0 || index + count > Count) throw new ArgumentOutOfRangeException(nameof(count));
+
+            // 从最后往前删除，这样不需要移动元素
+            for (int i = index + count - 1; i >= index; i--)
+                RemoveAt(i);
+        }
+
+        /// <summary>在指定的索引处将集合的元素插入 PooledList&lt;T&gt;</summary>
+        /// <param name="index">应在此处插入新元素的从零开始的索引</param>
+        /// <param name="collection">要插入的集合，集合本身不能为 null，但它可以包含为 null 的元素</param>
+        /// <exception cref="ArgumentNullException">collection 为 null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">index 小于 0 或大于 Count</exception>
+        public void InsertRange(int index, IEnumerable<T> collection)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if ((uint)index > (uint)Count) throw new ArgumentOutOfRangeException(nameof(index));
+
+            foreach (var item in collection)
+                Insert(index++, item);
+        }
+
+        /// <summary>将指定数量的元素从源数组复制到 PooledList&lt;T&gt;</summary>
+        /// <param name="source">要从中复制元素的源数组</param>
+        /// <param name="sourceIndex">源数组中开始复制的索引</param>
+        /// <param name="destinationIndex">PooledList&lt;T&gt; 中开始粘贴的索引</param>
+        /// <param name="count">要复制的元素数</param>
+        /// <exception cref="ArgumentNullException">source 为 null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">sourceIndex、destinationIndex 或 count 超出范围</exception>
+        /// <exception cref="ArgumentException">源数组中没有足够的元素可复制</exception>
+        public void CopyFrom(T[] source, int sourceIndex, int destinationIndex, int count)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (sourceIndex < 0) throw new ArgumentOutOfRangeException(nameof(sourceIndex));
+            if (destinationIndex < 0 || destinationIndex > Count) throw new ArgumentOutOfRangeException(nameof(destinationIndex));
+            if (count < 0 || sourceIndex + count > source.Length || destinationIndex + count > Count) throw new ArgumentOutOfRangeException(nameof(count));
+
+            for (int i = 0; i < count; i++)
+                this[destinationIndex + i] = source[sourceIndex + i];
+        }
+        #endregion
+    }
+}
---
+++ OwBaseCore/System.Collections.Generic/PooledListBase.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// 基于 ArrayPool 池化数组的列表基类，实现 IList&lt;T&gt; 接口
+    /// </summary>
+    /// <typeparam name="T">列表中的元素类型</typeparam>
+    public class PooledListBase<T> : IList<T>, IDisposable
+    {
+        internal T[] _Items;          // 存储元素的池化数组
+        private int _Count;           // 列表中当前元素数量
+        private bool _isDisposed;    // 对象是否已被释放
+        private const int DefaultCapacity = 8;  // 默认初始容量
+
+        /// <summary>
+        /// 使用默认容量初始化 PooledListBase 实例
+        /// </summary>
+        public PooledListBase()
+        {
+            _Items = ArrayPool<T>.Shared.Rent(DefaultCapacity);
+            _Count = 0;
+        }
+
+        /// <summary>
+        /// 使用指定的初始容量初始化 PooledListBase 实例
+        /// </summary>
+        /// <param name="capacity">初始容量</param>
+        /// <exception cref="ArgumentOutOfRangeException">capacity 小于 0 时抛出</exception>
+        public PooledListBase(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity), "容量不能为负数");
+
+            _Items = capacity == 0 ? Array.Empty<T>() : ArrayPool<T>.Shared.Rent(capacity);
+            _Count = 0;
+        }
+
+        #region IList<T> 和 ICollection<T> 接口实现
+
+        /// <summary>
+        /// 列表中的元素数量
+        /// </summary>
+        public int Count => _Count;
+
+        /// <summary>
+        /// 列表是否为只读（始终返回 false）
+        /// </summary>
+        public bool IsReadOnly => false;
+
+        /// <summary>
+        /// 获取或设置指定索引处的元素
+        /// </summary>
+        /// <param name="index">要获取或设置的元素的索引</param>
+        /// <returns>指定索引处的元素</returns>
+        /// <exception cref="ArgumentOutOfRangeException">index 小于 0 或大于等于 Count</exception>
+        public T this[int index]
+        {
+            get
+            {
+                if ((uint)index >= (uint)_Count)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+
+                return _Items[index];
+            }
+            set
+            {
+                if ((uint)index >= (uint)_Count)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+
+                _Items[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// 将元素添加到列表末尾
+        /// </summary>
+        /// <param name="item">要添加的元素</param>
+        public void Add(T item)
+        {
+            if (_Count == _Items.Length)
+            {
+                EnsureCapacity(_Count + 1);
+            }
+
+            _Items[_Count++] = item;
+        }
+
+        /// <summary>
+        /// 在指定索引处插入元素
+        /// </summary>
+        /// <param name="index">要插入元素的位置</param>
+        /// <param name="item">要插入的元素</param>
+        /// <exception cref="ArgumentOutOfRangeException">index 小于 0 或大于 Count</exception>
+        public void Insert(int index, T item)
+        {
+            if ((uint)index > (uint)_Count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (_Count == _Items.Length)
+            {
+                EnsureCapacity(_Count + 1);
+            }
+
+            if (index < _Count)
+            {
+                Array.Copy(_Items, index, _Items, index + 1, _Count - index);
+            }
+
+            _Items[index] = item;
+            _Count++;
+        }
+
+        /// <summary>
+        /// 从列表中移除指定元素的第一个匹配项
+        /// </summary>
+        /// <param name="item">要移除的元素</param>
+        /// <returns>如果成功移除元素，则为 true；否则为 false</returns>
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 移除指定索引处的元素
+        /// </summary>
+        /// <param name="index">要移除的元素的索引</param>
+        /// <exception cref="ArgumentOutOfRangeException">index 小于 0 或大于等于 Count</exception>
+        public void RemoveAt(int index)
+        {
+            if ((uint)index >= (uint)_Count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            _Count--;
+
+            if (index < _Count)
+            {
+                Array.Copy(_Items, index + 1, _Items, index, _Count - index);
+            }
+
+            // 防止保留引用
+            if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
+            {
+                _Items[_Count] = default;
+            }
+        }
+
+        /// <summary>
+        /// 从列表中移除所有元素
+        /// </summary>
+        public void Clear()
+        {
+            if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
+            {
+                Array.Clear(_Items, 0, _Count);
+            }
+
+            _Count = 0;
+        }
+
+        /// <summary>
+        /// 返回元素在列表中首次出现的索引
+        /// </summary>
+        /// <param name="item">要在列表中查找的元素</param>
+        /// <returns>如果找到元素，则为该元素的索引；否则为 -1</returns>
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(_Items, item, 0, _Count);
+        }
+
+        /// <summary>
+        /// 确定列表是否包含特定元素
+        /// </summary>
+        /// <param name="item">要在列表中查找的元素</param>
+        /// <returns>如果在列表中找到该元素，则为 true；否则为 false</returns>
+        public bool Contains(T item)
+        {
+            return IndexOf(item) >= 0;
+        }
+
+        /// <summary>
+        /// 将列表的元素复制到数组中
+        /// </summary>
+        /// <param name="array">作为复制目标的一维数组</param>
+        /// <param name="arrayIndex">array 中从零开始的索引，从此处开始复制</param>
+        /// <exception cref="ArgumentNullException">array 为 null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">arrayIndex 小于 0</exception>
+        /// <exception cref="ArgumentException">源中的元素数目超出从 arrayIndex 到目标数组末尾的可用空间</exception>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (arrayIndex < 0 || arrayIndex > array.Length - _Count)
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+
+            Array.Copy(_Items, 0, array, arrayIndex, _Count);
+        }
+
+        /// <summary>
+        /// 返回循环访问集合的枚举器
+        /// </summary>
+        /// <returns>可用于循环访问集合的枚举器</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _Count; i++)
+            {
+                yield return _Items[i];
+            }
+        }
+
+        /// <summary>
+        /// 返回循环访问集合的枚举器
+        /// </summary>
+        /// <returns>可用于循环访问集合的枚举器</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 列表的当前容量，只读属性
+        /// </summary>
+        public int Capacity => _Items.Length;
+
+        /// <summary>
+        /// 调整列表的容量大小
+        /// </summary>
+        /// <param name="newCapacity">新的容量大小</param>
+        /// <exception cref="ArgumentOutOfRangeException">newCapacity 小于当前元素数量</exception>
+        public void Resize(int newCapacity)
+        {
+            if (newCapacity < _Count)
+                throw new ArgumentOutOfRangeException(nameof(newCapacity), "新容量小于当前元素数量");
+
+            if (newCapacity != _Items.Length)
+            {
+                if (newCapacity > 0)
+                {
+                    T[] newItems = ArrayPool<T>.Shared.Rent(newCapacity);
+                    if (newItems.Length == _Items.Length) //直接返回原数组，避免不必要的内存分配
+                    {
+                        ArrayPool<T>.Shared.Return(newItems);
+                        return;
+                    }
+                    if (_Count > 0)
+                    {
+                        Array.Copy(_Items, newItems, _Count);
+                    }
+
+                    ReturnArray();
+                    _Items = newItems;
+                }
+                else
+                {
+                    ReturnArray();
+                    _Items = Array.Empty<T>();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 将内部数组归还到数组池
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void ReturnArray()
+        {
+            // 使用 Interlocked.Exchange 原子地替换 _items 并获取旧值
+            T[] toReturn = Interlocked.Exchange(ref _Items, Array.Empty<T>());
+
+            // 如果没有数组需要返回，直接退出
+            if (toReturn == null || toReturn.Length == 0)
+                return;
+
+            try
+            {
+                // 直接使用 clearArray: true 让 ArrayPool 内部处理清理工作
+                ArrayPool<T>.Shared.Return(toReturn, clearArray: true);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"在归还数组到 ArrayPool 时出错: {ex.Message}");
+            }
+        }
+
+        #region IDisposable 接口实现
+
+        /// <summary>
+        /// 释放由 PooledListBase 使用的所有资源
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// 释放由 PooledListBase 使用的非托管资源，并指示是否也应释放托管资源
+        /// </summary>
+        /// <param name="disposing">如果应该释放托管资源，则为 true；否则为 false</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    // 释放托管资源
+                    ReturnArray();
+                }
+
+                // 释放非托管资源
+                _isDisposed = true;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 将现有集合中的元素添加到列表的末尾
+        /// </summary>
+        /// <param name="collection">要添加的集合</param>
+        /// <exception cref="ArgumentNullException">collection 为 null</exception>
+        public void AddRange(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            if (collection is ICollection<T> c)
+            {
+                int count = c.Count;
+                if (count > 0)
+                {
+                    EnsureCapacity(_Count + count);
+                    c.CopyTo(_Items, _Count);
+                    _Count += count;
+                }
+            }
+            else
+            {
+                foreach (var item in collection)
+                {
+                    Add(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 对列表执行操作以减少未使用的内存
+        /// </summary>
+        public void TrimExcess()
+        {
+            if (_Count < _Items.Length * 0.9)
+            {
+                Resize(_Count);
+            }
+        }
+
+        /// <summary>
+        /// 确保列表具有足够的容量来容纳指定数量的元素
+        /// </summary>
+        /// <param name="min">所需的最小容量</param>
+        public void EnsureCapacity(int min)
+        {
+            if (_Items.Length < min)
+            {
+                int newCapacity = _Items.Length == 0 ? DefaultCapacity : _Items.Length * 2;
+
+                if (newCapacity < min)
+                    newCapacity = min;
+
+                Resize(newCapacity);
+            }
+        }
+
+    }
+}
