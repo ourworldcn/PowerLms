cb632d789003409c00b760821542a0df63d9633a
ourworld_cn <ourworld_cn@msn.com>
2025-06-04 22:45:12 +0800

    修改注释

--- OwBaseCore/PasswordGenerator.cs
+++ OwBaseCore/PasswordGenerator.cs
@@ -46,7 +46,7 @@ namespace OW
         /// 生成指定长度的随机密码。
         /// </summary>
         /// <param name="length">密码长度</param>
-        /// <param name="includeSpecialChars">是否包含特殊字符，"!@#$%^&*()_+-=[]{}|;:,./?"</param>
+        /// <param name="includeSpecialChars">是否包含特殊字符（如 !@#$%^&amp;*()_+-=[]{}|;:,./?）</param>
         /// <returns>生成的随机密码，默认情况下是大小写英文字母和数字，但无l、o、I、O、0、1以避免混淆。</returns>
         /// <exception cref="ArgumentException">密码长度必须大于0</exception>
         public string Generate(int length, bool includeSpecialChars = false)
