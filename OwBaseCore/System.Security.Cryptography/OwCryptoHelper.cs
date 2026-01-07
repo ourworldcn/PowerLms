ce531abe3f2e72099f4efe097b84723be91ea1af
ourworld_cn <ourworld_cn@msn.com>
2026-01-07 17:32:33 +0800

    精简AES Gcm安全帮助类

--- OwBaseCore/System.Security.Cryptography/OwCryptoHelper.cs
+++ OwBaseCore/System.Security.Cryptography/OwCryptoHelper.cs
@@ -13,34 +13,39 @@ using System.Text;
 namespace OW.Security
 {
     /// <summary>
-    /// 加密解密工具类 - AES-256-GCM实现
+    /// 加密解密工具类 - AES-GCM实现
     /// </summary>
     /// <remarks>
-    /// <para>加密格式: Nonce[12] + Tag[16] + Cipher[变长], 总开销28字节</para>
+    /// <para>支持AES-128/192/256-GCM，密钥长度分别为16/24/32字节</para>
+    /// <para>自定义加密格式: Nonce[12] + Tag[16] + Cipher[变长], 总开销28字节</para>
     /// <para>返回ArraySegment&lt;byte&gt;包含ArrayPool数组引用及有效范围(零拷贝)</para>
     /// <para>使用完毕后可选择性归还: ArrayPool&lt;byte&gt;.Shared.Return(segment.Array)</para>
+    /// <para>WARNING: 加密和解密方法使用自定义数据格式，必须配对使用，不可与其他AES-GCM实现互操作</para>
     /// </remarks>
     public static class OwCryptoHelper
     {
-        #region 二进制数据加解密
+        #region AES-GCM 二进制数据加解密
 
         private static readonly int NonceSize = AesGcm.NonceByteSizes.MinSize;  // 12字节(96位)
         private static readonly int TagSize = AesGcm.TagByteSizes.MaxSize;      // 16字节(128位)
         private static readonly int Overhead = NonceSize + TagSize;             // 28字节
 
         /// <summary>
-        /// 使用AES-256-GCM加密二进制数据
+        /// 使用AES-GCM加密二进制数据
         /// </summary>
         /// <param name="plainData">待加密的明文数据</param>
-        /// <param name="key">加密密钥(必须32字节)</param>
+        /// <param name="key">加密密钥(支持16/24/32字节,分别对应AES-128/192/256)</param>
         /// <returns>ArraySegment包含ArrayPool数组引用(Array)及有效范围(Offset=0, Count=实际长度)</returns>
-        /// <exception cref="ArgumentException">key长度不为32字节时抛出</exception>
-        public static ArraySegment<byte> Encrypt(ReadOnlySpan<byte> plainData, ReadOnlySpan<byte> key)
+        /// <exception cref="ArgumentException">key长度不为16/24/32字节时抛出</exception>
+        /// <remarks>
+        /// <para>自定义加密格式: Nonce[12] + Tag[16] + Cipher[变长]，必须使用本类的DecryptAesGcm方法解密</para>
+        /// <para>空数据处理: plainData.Length == 0 时仍会加密，返回28字节数据(Nonce[12] + Tag[16])</para>
+        /// <para>WARNING: 加密结果使用自定义格式，不可与其他AES-GCM实现互操作，必须配对使用本类的加解密方法</para>
+        /// </remarks>
+        public static ArraySegment<byte> EncryptAesGcm(ReadOnlySpan<byte> plainData, ReadOnlySpan<byte> key)
         {
-            if (key.Length != 32)
-                throw new ArgumentException("密钥长度必须为32字节(AES-256)", nameof(key));
-            if (plainData.Length == 0)
-                return ArraySegment<byte>.Empty;
+            if (key.Length != 16 && key.Length != 24 && key.Length != 32)
+                throw new ArgumentException("密钥长度必须为16/24/32字节(AES-128/192/256)", nameof(key));
             var requiredLength = plainData.Length + Overhead;
             var result = ArrayPool<byte>.Shared.Rent(requiredLength);
             using var aesGcm = new AesGcm(key);
@@ -54,22 +59,31 @@ namespace OW.Security
         }
 
         /// <summary>
-        /// 使用AES-256-GCM解密二进制数据
+        /// 使用AES-GCM解密二进制数据
         /// </summary>
-        /// <param name="encryptedData">加密数据(格式: Nonce[12] + Tag[16] + Cipher[变长])</param>
-        /// <param name="key">解密密钥(必须32字节)</param>
+        /// <param name="encryptedData">加密数据(格式: Nonce[12] + Tag[16] + Cipher[变长])，必须由本类的EncryptAesGcm方法生成</param>
+        /// <param name="key">解密密钥(支持16/24/32字节,分别对应AES-128/192/256)</param>
         /// <returns>ArraySegment包含ArrayPool数组引用(Array)及有效范围(Offset=0, Count=实际长度)</returns>
-        /// <exception cref="ArgumentException">key长度不为32字节或数据格式无效时抛出</exception>
+        /// <exception cref="ArgumentException">key长度不为16/24/32字节或数据格式无效时抛出</exception>
         /// <exception cref="InvalidOperationException">数据被篡改或损坏时抛出</exception>
-        public static ArraySegment<byte> Decrypt(ReadOnlySpan<byte> encryptedData, ReadOnlySpan<byte> key)
+        /// <remarks>
+        /// <para>自定义解密格式: 仅支持本类EncryptAesGcm方法生成的数据(Nonce[12] + Tag[16] + Cipher[变长])</para>
+        /// <para>空数据处理: encryptedData.Length == 28 时解密为空数组(Array.Empty&lt;byte&gt;())，encryptedData.Length &lt; 28 时抛出ArgumentException</para>
+        /// <para>WARNING: 仅能解密本类加密方法生成的数据，不可用于其他AES-GCM实现的密文，必须配对使用</para>
+        /// </remarks>
+        public static ArraySegment<byte> DecryptAesGcm(ReadOnlySpan<byte> encryptedData, ReadOnlySpan<byte> key)
         {
-            if (key.Length != 32)
-                throw new ArgumentException("密钥长度必须为32字节(AES-256)", nameof(key));
-            if (encryptedData.Length == 0)
-                return ArraySegment<byte>.Empty;
+            if (key.Length != 16 && key.Length != 24 && key.Length != 32)
+                throw new ArgumentException("密钥长度必须为16/24/32字节(AES-128/192/256)", nameof(key));
             if (encryptedData.Length < Overhead)
                 throw new ArgumentException($"加密数据格式无效,最小长度应为{Overhead}字节", nameof(encryptedData));
             var plainLength = encryptedData.Length - Overhead;
+            if (plainLength == 0)
+            {
+                using var aesGcm = new AesGcm(key);
+                aesGcm.Decrypt(encryptedData.Slice(0, NonceSize), ReadOnlySpan<byte>.Empty, encryptedData.Slice(NonceSize, TagSize), Span<byte>.Empty);
+                return new ArraySegment<byte>(Array.Empty<byte>());
+            }
             var result = ArrayPool<byte>.Shared.Rent(plainLength);
             try
             {
@@ -84,118 +98,28 @@ namespace OW.Security
             }
         }
 
-        #endregion 二进制数据加解密
-
-        #region 字符串加解密
-
-        /// <summary>
-        /// 加密字符串(UTF8编码)
-        /// </summary>
-        /// <param name="plainText">待加密的明文字符串</param>
-        /// <param name="key">加密密钥(必须32字节)</param>
-        /// <returns>Base64编码的加密字符串</returns>
-        /// <exception cref="ArgumentNullException">plainText为null时抛出</exception>
-        /// <exception cref="ArgumentException">key长度不为32字节时抛出</exception>
-        public static string EncryptString(string plainText, ReadOnlySpan<byte> key)
-        {
-            ArgumentNullException.ThrowIfNull(plainText);
-            if (plainText.Length == 0) return string.Empty;
-            var plainBytes = Encoding.UTF8.GetBytes(plainText);
-            var encrypted = Encrypt(plainBytes, key);
-            try
-            {
-                return Convert.ToBase64String(encrypted.Array, encrypted.Offset, encrypted.Count);
-            }
-            finally
-            {
-                ArrayPool<byte>.Shared.Return(encrypted.Array);
-            }
-        }
-
-        /// <summary>
-        /// 解密字符串(UTF8编码)
-        /// </summary>
-        /// <param name="encryptedText">Base64编码的加密字符串</param>
-        /// <param name="key">解密密钥(必须32字节)</param>
-        /// <returns>解密后的明文字符串</returns>
-        /// <exception cref="ArgumentNullException">encryptedText为null时抛出</exception>
-        /// <exception cref="ArgumentException">key长度不为32字节或Base64格式无效时抛出</exception>
-        /// <exception cref="InvalidOperationException">数据被篡改或损坏时抛出</exception>
-        public static string DecryptString(string encryptedText, ReadOnlySpan<byte> key)
-        {
-            ArgumentNullException.ThrowIfNull(encryptedText);
-            if (encryptedText.Length == 0) return string.Empty;
-            byte[] encryptedBytes;
-            try
-            {
-                encryptedBytes = Convert.FromBase64String(encryptedText);
-            }
-            catch (FormatException ex)
-            {
-                throw new ArgumentException("加密文本的Base64格式无效", nameof(encryptedText), ex);
-            }
-            var plainBytes = Decrypt(encryptedBytes, key);
-            try
-            {
-                return Encoding.UTF8.GetString(plainBytes.Array, plainBytes.Offset, plainBytes.Count);
-            }
-            finally
-            {
-                ArrayPool<byte>.Shared.Return(plainBytes.Array);
-            }
-        }
-
-        #endregion 字符串加解密
+        #endregion AES-GCM 二进制数据加解密
 
         #region 密钥管理工具
 
         /// <summary>
-        /// 生成新的AES-256密钥(32字节)
+        /// 生成指定长度的AES-GCM密钥
         /// </summary>
-        /// <returns>32字节的随机密钥</returns>
-        public static byte[] GenerateKey()
+        /// <param name="keySizeInBytes">密钥长度(字节)，有效值: 16(AES-128)、24(AES-192)、32(AES-256)</param>
+        /// <returns>指定长度的随机密钥</returns>
+        /// <exception cref="ArgumentException">密钥长度不为16/24/32字节时抛出</exception>
+        /// <remarks>
+        /// 密钥长度对应关系:
+        /// - 16字节 = 128位 = AES-128-GCM
+        /// - 24字节 = 192位 = AES-192-GCM
+        /// - 32字节 = 256位 = AES-256-GCM (推荐，最高安全性)
+        /// </remarks>
+        public static byte[] GenerateKey(int keySizeInBytes = 32)
         {
-            var key = new byte[32];
+            if (keySizeInBytes != 16 && keySizeInBytes != 24 && keySizeInBytes != 32)
+                throw new ArgumentException("密钥长度必须为16/24/32字节(AES-128/192/256)", nameof(keySizeInBytes));
+            var key = new byte[keySizeInBytes];
             RandomNumberGenerator.Fill(key);
-            return key;
-        }
-
-        /// <summary>
-        /// 将密钥转换为Base64字符串
-        /// </summary>
-        /// <param name="key">32字节密钥</param>
-        /// <returns>Base64编码的密钥字符串</returns>
-        /// <exception cref="ArgumentNullException">key为null时抛出</exception>
-        /// <exception cref="ArgumentException">key长度不为32字节时抛出</exception>
-        public static string KeyToBase64(byte[] key)
-        {
-            ArgumentNullException.ThrowIfNull(key);
-            if (key.Length != 32)
-                throw new ArgumentException("密钥长度必须为32字节", nameof(key));
-            return Convert.ToBase64String(key);
-        }
-
-        /// <summary>
-        /// 从Base64字符串解析密钥
-        /// </summary>
-        /// <param name="base64Key">Base64编码的密钥字符串</param>
-        /// <returns>32字节密钥</returns>
-        /// <exception cref="ArgumentNullException">base64Key为null时抛出</exception>
-        /// <exception cref="ArgumentException">Base64格式无效或长度不为32字节时抛出</exception>
-        public static byte[] KeyFromBase64(string base64Key)
-        {
-            ArgumentNullException.ThrowIfNull(base64Key);
-            byte[] key;
-            try
-            {
-                key = Convert.FromBase64String(base64Key);
-            }
-            catch (FormatException ex)
-            {
-                throw new ArgumentException("Base64密钥格式无效", nameof(base64Key), ex);
-            }
-            if (key.Length != 32)
-                throw new ArgumentException($"密钥长度必须为32字节,实际为{key.Length}字节", nameof(base64Key));
             return key;
         }
 
